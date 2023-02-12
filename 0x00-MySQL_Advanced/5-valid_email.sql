0c7090078aa398c7298fb3bbf7056cd4e174b866
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-12 15:07:47 +0000

    validate email in sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,10 @@ ON users
 FOR EACH ROW
 BEGIN
 SET NEW.valid_email = CASE
-    WHEN OLD.valid_email = 0 AND  NEW.email != OLD.email THEN 1
-    WHEN OLD.valid_email = 1 AND  NEW.email != OLD.email THEN 0
+    WHEN OLD.valid_email = 0 AND  NEW.email != OLD.email AND NEW.email REGEXP '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*(\\.[a-zA-Z]{2,4})+$'
+    THEN 1
+    WHEN OLD.valid_email = 1 AND  NEW.email != OLD.email AND NEW.email NOT REGEXP '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*(\\.[a-zA-Z]{2,4})+$'
+    THEN 0
     ELSE NEW.valid_email
 END;
 END;
