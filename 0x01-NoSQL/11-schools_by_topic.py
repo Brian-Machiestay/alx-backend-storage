754e1919d94fc274b489d0e91e4c1cf6ebf1a209
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-15 08:29:39 +0000

    schools by topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,6 +7,8 @@ def schools_by_topic(mongo_collection, topic):
     schools = []
     result = mongo_collection.find()
     for doc in result:
-        if topic in doc.topics:
-            schools.append(doc.name)
+        if 'topics' in doc.keys():
+            if topic in doc['topics']:
+                if 'name' in doc.keys():
+                    schools.append(doc)
     return schools
