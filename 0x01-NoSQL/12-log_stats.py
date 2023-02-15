c9840d21a53629b5a41cd69d84c8319256007e9a
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-15 10:11:03 +0000

    work on mongo dump

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""log nginx stats"""
+
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    nginx = client.logs.nginx
+    number_docs = nginx.count_documents({})
+    get = nginx.count_documents({"method": "GET"})
+    post = nginx.count_documents({"method": "POST"})
+    put = nginx.count_documents({"method": "PUT"})
+    patch = nginx.count_documents({"method": "PATCH"})
+    delete = nginx.count_documents({"method": "DELETE"})
+    stat = nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} logs".format(number_docs))
+    print("methods:")
+    print("\tmethod GET: {}".format(get))
+    print("\tmethod POST: {}".format(post))
+    print("\tmethod PUT: {}".format(put))
+    print("\tmethod PATCH: {}".format(patch))
+    print("\tmethod DELETE: {}".format(delete))
+    print("{} status check".format(stat))
