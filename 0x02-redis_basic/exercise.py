10aebc865b8f4a1696e33e7ce59b4615e9e3a52f
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-16 13:45:39 +0000

    streamline annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, float, bytes]) -> str:
+    def store(self, data: Union[str, float, bytes]) -> str:
         """store data into redis db"""
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
