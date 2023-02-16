2c4279814a5354435a8dde0fb24564a35de13d11
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-16 13:32:17 +0000

    correct annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 
 import redis
-from typing import Any
+from typing import Any, Union
 import uuid
 
 
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         """store data into redis db"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
