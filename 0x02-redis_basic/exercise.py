7b8066bcba21ab5fa06441084986543913268d86
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-17 11:26:08 +0000

    count function calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,17 @@
 import redis
 from typing import Any, Union, Callable
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """a decorator function that counts calls to Cache"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """the wrapper class"""
+        args[0]._redis.incr(method.__qualname__)
+        return method(*args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -14,12 +25,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, float, bytes]) -> str:
         """store data into redis db"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return(key)
 
+    @count_calls
     def get(self, key: str, fn: Callable = None) -> Any:
         """convert a value to a custom type"""
         val = self._redis.get(key)
@@ -28,10 +41,12 @@ class Cache:
         val = fn(val)
         return val
 
+    @count_calls
     def get_str(self) -> Callable:
         """return the conversion function for a string"""
         return lambda x: str(x)
 
+    @count_calls
     def get_int(self) -> Callable:
         """return the conversion function for an int"""
         return lambda x: int(x)
