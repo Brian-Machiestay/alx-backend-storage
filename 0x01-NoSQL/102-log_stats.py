69fe8c911dce0df3fed4c1788abcde4c772839ca
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-15 07:18:10 +0000

    display ip stats

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""log nginx stats"""
+
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    nginx = client.logs.nginx
+    number_docs = nginx.count_documents({})
+    get = nginx.count_documents({"method": "GET"})
+    post = nginx.count_documents({"method": "POST"})
+    put = nginx.count_documents({"method": "PUT"})
+    patch = nginx.count_documents({"method": "PATCH"})
+    delete = nginx.count_documents({"method": "DELETE"})
+    stat = nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} logs".format(number_docs))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get))
+    print("\tmethod POST: {}".format(post))
+    print("\tmethod PUT: {}".format(put))
+    print("\tmethod PATCH: {}".format(patch))
+    print("\tmethod DELETE: {}".format(delete))
+    print("{} status check".format(stat))
+
+    pipeline = [
+        {
+            '$group': {
+                '_id': '$ip',
+                'count': {'$sum': 1}
+            }
+        },
+        {'$sort': {
+            'count': -1
+        }},
+        {'$limit': 10}
+    ]
+
+    result = nginx.aggregate(pipeline)
+    print('IPs:')
+    for res in result:
+        print('\t{}: {}'.format(res['_id'], res['count']))
