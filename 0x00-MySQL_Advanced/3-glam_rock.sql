57ec506d1a97c5cce26ace75fde3e9e046d5d4be
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-15 09:31:49 +0000

    get glam rock style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name FROM metal_bands
-WHERE 
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
