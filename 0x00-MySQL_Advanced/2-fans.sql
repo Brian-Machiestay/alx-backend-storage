aca62c0c17271ebf26c611130f1db5c5c6774eb9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-15 09:12:10 +0000

    rantk countries by fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- rank country origin of bands ordered by number of fans
-SELECT origin, 0 + fans AS nb_fans FROM metal_bands
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans
+ORDER BY nb_fans DESC;
